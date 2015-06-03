b2d5f458e3057294c37d60223e7ebef1b70a6c50
App Academy Student <student@appacademy.io>
2015-06-03 11:17:08 -0700

    initialize repository

---
+++ app/assets/javascripts/journal.js
@@ -0,0 +1,13 @@
+window.Journal = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  Journal.initialize();
+});
